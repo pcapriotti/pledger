4f48a122fd741792c14c03babb0db3dc1ec43238
Paolo Capriotti <p.capriotti@gmail.com>
2011-05-27 14:34:29 +0100

    Add RuleFilter.

--- pledger/filters.py
+++ pledger/filters.py
@@ -27,12 +27,54 @@ class FilterCollection(object):
 class Filter(object):
     pass
 
-class AccountFilter(Filter):
-    def __init__(self, account):
-        self.account = account
+class Predicate(object):
+    def __init__(self, predicate):
+        self.predicate = predicate
+
+    def __call__(self, *args):
+        return self.predicate(*args)
+
+    def __and__(self, other):
+        @Predicate
+        def result(*args):
+            return self(*args) and other(*args)
+        return result
+
+    def __or__(self, other):
+        @Predicate
+        def result(*args):
+            return self(*args) or other(*args)
+        return result
+
+class Generator(object):
+    def __init__(self, generator):
+        self.generator = generator
+
+    def __call__(self, *args):
+        return self.generator(*args)
+
+    def __add__(self, other):
+        @Generator
+        def result(*args):
+            return self(*args) + other(*args)
+        return result
+Generator.identity = Generator(lambda x: [x])
+Generator.null = Generator(lambda x: [])
+
+class RuleFilter(Filter):
+    def __init__(self, predicate, generator, complement = Generator.identity):
+        self.predicate = predicate
+        self.generators = {
+                True : generator,
+                False : complement }
 
     def apply(self, transaction, entry):
-        if entry.account == self.account:
-            return [entry]
-        else:
-            return []
+        gen = self.generators[self.predicate(transaction, entry)]
+        return gen(entry)
+
+class AccountFilter(RuleFilter):
+    def __init__(self, account):
+        @Predicate
+        def predicate(transaction, entry):
+            return entry.account == account
+        super(AccountFilter, self).__init__(predicate, Generator.identity, Generator.null)
