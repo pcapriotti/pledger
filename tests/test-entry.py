9f31296b1b5c3116e22ea6d896669f10b4c56747
Paolo Capriotti <p.capriotti@gmail.com>
2011-06-04 21:41:58 +0100

    Fix unit tests.

--- tests/test-entry.py
+++ tests/test-entry.py
@@ -1,12 +1,14 @@
 from pledger.entry import Entry
-from pledger.account import Account
-from pledger.value import Value
+from pledger.parser import Parser
 
 import unittest
 
 class EntryTest(unittest.TestCase):
+    def setUp(self):
+        self.parser = Parser()
+
     def testParsing(self):
-        value = Value.parse("34 EUR")
-        account = Account.parse("Test Account")
+        value = self.parser.parse_value("34 EUR")
+        account = self.parser.parse_account("Test Account")
         entry = Entry(account, value)
-        self.assertEqual(entry, Entry.parse("  Test Account        34 EUR"))
+        self.assertEqual(entry, self.parser.parse_entry("  Test Account        34 EUR"))
--- tests/test-transaction.py
+++ tests/test-transaction.py
@@ -1,18 +1,21 @@
 import unittest
-from pledger.account import Account
 from pledger.transaction import Transaction
 from pledger.value import Value
 from pledger.entry import Entry
+from pledger.parser import Parser
 
 class TransactionTest(unittest.TestCase):
+    def setUp(self):
+        self.parser = Parser()
+
     def testParsing(self):
         str = "2011/04/15 * Bookshop\n" + \
               "    Assets:Bank       -40 EUR\n" + \
               "    Expenses:Books"
-        bank = Account.parse("Assets:Bank")
-        expenses = Account.parse("Expenses:Books")
-        value = Value.parse("40 EUR")
+        bank = self.parser.parse_account("Assets:Bank")
+        expenses = self.parser.parse_account("Expenses:Books")
+        value = self.parser.parse_value("40 EUR")
         entry1 = Entry(bank, -value)
         entry2 = Entry(expenses, value)
         tr = Transaction([entry1, entry2])
-        self.assertEquals(tr, Transaction.parse(str))
+        self.assertEquals(tr, self.parser.parse_transaction(str))
