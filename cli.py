77cecbaf6ab20bb1278123ec9204820a3ab27bb2
Paolo Capriotti <p.capriotti@gmail.com>
2011-06-09 15:49:23 +0100

    Add pattern arguments.

--- cli.py
+++ cli.py
@@ -5,6 +5,7 @@ from pledger.parser import Parser
 from pledger.sorting import MapSorting
 from pledger.rule import RuleCollection
 from pledger.template import default_template
+import re, os, sys
 
 parser = Parser()
 filter = Filter.null
@@ -13,16 +14,31 @@ sorting = MapSorting(lambda x: x.transaction.date)
 template = default_template
 
 def run_cli():
+    global filter
+
     argparser = ArgumentParser()
     argparser.add_argument("report", action="store")
     argparser.add_argument("--filename", action="store", nargs=1)
+    argparser.add_argument("patterns", metavar="PATTERN", type=str,
+                           nargs="*")
 
     args = argparser.parse_args()
 
     report_factory = reports.get(args.report)
     if report_factory is None:
         raise Exception("no such report " + args.report)
+    filters = [Filter.matches(re.compile(pattern)) for pattern in args.patterns]
+    if filters:
+        filter &= reduce(Filter.__or__, filters)
 
-    ledger = parser.parse_ledger(open(args.filename[0]).read())
+    filename = os.environ.get("PLEDGER")
+    if filename is None:
+        filename = args.filename[0]
+
+    if filename is None:
+        sys.stderr.write("No ledger specified\n")
+        sys.exit(1)
+
+    ledger = parser.parse_ledger(open(filename).read())
     report = report_factory(ledger, rules, filter, sorting)
     print template(report)
