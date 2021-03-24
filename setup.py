81dee9a7e091e5c95830e04bc41daa0cdb0f5252
Paolo Capriotti <paolo@capriotti.io>
2021-03-24 18:28:38 +0100

    Update packaging

--- setup.py
+++ setup.py
@@ -1,38 +1,27 @@
 #!/usr/bin/env python
 
-# Copyright (C) 2011 by Paolo Capriotti <p.capriotti@gmail.com>
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
+import os
+import setuptools
 
-from setuptools import setup, find_packages
-from pledger.version import get_version
+APP_ROOT = os.path.dirname(__file__)
+README = os.path.join(APP_ROOT, 'README.md')
+exec(open(os.path.join(APP_ROOT, 'pledger', '_version.py')).read())
 
-setup(name="pledger",
-      version=get_version(),
-      description="Command line accounting tool and library",
-      author="Paolo Capriotti",
-      author_email="p.capriotti@gmail.com",
-      url="https://github.com/pcapriotti/pledger",
-      packages=find_packages(),
-      test_suite='nose.collector',
-      tests_require=['nose', 'mock'],
-      entry_points={
-          'console_scripts': [
-              'plg = pledger.cli:run_cli' ] },
-      license="MIT")
+setuptools.setup(
+    name='pledger',
+    version=__version__,
+    description="Command line accounting tool and library",
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url="https://github.com.com/pcapriotti/pledger",
+    author="Paolo Capriotti",
+    author_email="paolo@capriotti.io",
+    license="MIT",
+    packages=["pledger"],
+    scripts=[
+        'bin/pledger',
+    ],
+    python_requires='>= 3.7',
+    install_requires=[
+    ]
+)
