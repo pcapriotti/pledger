4cdb7d690682cf01ffb81456f44e37d56e64111f
Paolo Capriotti <paolo@capriotti.io>
2021-03-25 16:22:43 +0100

    Port test_util to pytest

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,65 +1,64 @@
-import unittest
 from pledger import util
+import pytest
 
-class UtilTest(unittest.TestCase):
-    def testItersplit(self):
-        items = list(range(20))
-        result =  [list(range(5)), list(range(6, 10)),
-                   list(range(11, 15)), list(range(16, 20))]
-        p = lambda x: x % 5 == 0
-        self.assertEqual(result, list(util.itersplit(p, items)))
 
-    def testObservable(self):
-        invocations = []
-        class Obs(util.Observable):
-            def foo(self):
-                self.fire("foo", 1)
-        class Listener(object):
-            def on_foo(inner, arg):
-                invocations.append(arg)
+def test_itersplit():
+    items = list(range(20))
+    result =  [list(range(5)), list(range(6, 10)),
+                list(range(11, 15)), list(range(16, 20))]
+    p = lambda x: x % 5 == 0
+    assert list(util.itersplit(p, items)) == result
 
-        obs = Obs()
-        listener = Listener()
-        obs.add_listener(listener)
-        obs.add_listener(object())
+def test_observable():
+    invocations = []
+    class Obs(util.Observable):
+        def foo(self):
+            self.fire("foo", 1)
+    class Listener(object):
+        def on_foo(inner, arg):
+            invocations.append(arg)
 
-        obs.foo()
+    obs = Obs()
+    listener = Listener()
+    obs.add_listener(listener)
+    obs.add_listener(object())
 
-        self.assertEqual([1], invocations)
+    obs.foo()
 
-    def testLinearized(self):
-        self.assertEqual([1,2,3], util.linearized([(1, []),(2, []),(3, [])]))
-        self.assertEqual([1,2,3,4,5,6],
-            util.linearized([
-                (1, [(2, []), (3, [(4, [])])]),
-                (5, []),
-                (6, [])]))
+    assert invocations == [1]
 
-class PrefixTreeTest(unittest.TestCase):
-    def setUp(self):
-        self.tree = util.PrefixTree()
+def test_linearized():
+    assert util.linearized([(1, []),(2, []),(3, [])]) == [1,2,3]
+    assert util.linearized([
+        (1, [(2, []), (3, [(4, [])])]),
+        (5, []),
+        (6, [])]) \
+        == [1,2,3,4,5,6]
 
-    def testEmpty(self):
-        self.assertEqual([], self.tree.from_prefix(""))
+@pytest.fixture
+def tree():
+    return util.PrefixTree()
 
-    def testUnambiguous(self):
-        self.tree.insert("hello")
-        self.tree.insert("world")
-        self.assertItemsEqual(["hello"], self.tree.from_prefix("h"))
+def test_empty(tree):
+    assert tree.from_prefix("") == []
 
-    def testAmbiguous(self):
-        self.tree.insert("hello")
-        self.tree.insert("help")
-        self.tree.insert("haste")
-        self.assertItemsEqual(["hello", "help"],
-                              self.tree.from_prefix("he"))
+def test_unambiguous(tree):
+    tree.insert("hello")
+    tree.insert("world")
+    assert tree.from_prefix("h") == ["hello"]
 
-    def testConstruction(self):
-        words = ["hello", "world"]
-        tree = util.PrefixTree(words)
-        self.assertItemsEqual(words, tree.from_prefix(""))
+def test_ambiguous(tree):
+    tree.insert("hello")
+    tree.insert("help")
+    tree.insert("haste")
+    assert tree.from_prefix("he") == ["hello", "help"]
 
-    def testFail(self):
-        self.tree.insert("hello")
-        self.assertItemsEqual([], self.tree.from_prefix("g"))
-        self.assertItemsEqual([], self.tree.from_prefix("help"))
+def test_construction():
+    words = ["hello", "world"]
+    tree = util.PrefixTree(words)
+    assert set(tree.from_prefix("")) == set(words)
+
+def test_fail(tree):
+    tree.insert("hello")
+    assert tree.from_prefix("g") == []
+    assert tree.from_prefix("help") == []
