ec7794e2fdee2e07053980249867a00408a3af1c
Paolo Capriotti <paolo@capriotti.io>
2021-03-25 15:58:51 +0100

    Port test_sorting to pytest

--- tests/test_sorting.py
+++ tests/test_sorting.py
@@ -1,37 +1,36 @@
-import unittest
 from pledger.sorting import Sorting, MapSorting, ExpressionSorting
 from pledger.parser import Parser
 from datetime import date
+import pytest
 
 
-class TestSorting(unittest.TestCase):
-    def setUp(self):
-        self.data = [3, 5, 1, 2, 7, 4, 9, 6, 0, 8]
-        self.parser = Parser()
+@pytest.fixture
+def data():
+    return [3, 5, 1, 2, 7, 4, 9, 6, 0, 8]
 
-    def testIdentitySorting(self):
-        identity = Sorting(lambda x: x)
-        self.assertEqual(self.data, identity(self.data))
+def test_identity_sorting(data):
+    identity = Sorting(lambda x: x)
+    assert data == identity(data)
 
-    def testSimpleSorting(self):
-        s = Sorting(lambda x: sorted(x))
-        self.assertEqual(list(range(10)), s(self.data))
+def test_simple_sorting(data):
+    s = Sorting(lambda x: sorted(x))
+    assert s(data) == list(range(10))
 
-    def testInvertedSorting(self):
-        s = ~Sorting(lambda x: sorted(x))
-        self.assertEqual(list(reversed(range(10))), s(self.data))
+def test_inverted_sorting(data):
+    s = ~Sorting(lambda x: sorted(x))
+    assert s(data) == list(reversed(range(10)))
 
-    def testMapSorting(self):
-        s = MapSorting(lambda x: x * (x - 4))
-        self.assertEqual([2, 3, 1, 4, 0, 5, 6, 7, 8, 9], s(self.data))
+def test_map_sorting(data):
+    s = MapSorting(lambda x: x * (x - 4))
+    assert s(data) == [2, 3, 1, 4, 0, 5, 6, 7, 8, 9]
 
-    def testExpressionSorting(self):
-        s = ExpressionSorting(self.parser, "record * (record - 4)")
-        self.assertEqual([2, 3, 1, 4, 0, 5, 6, 7, 8, 9], s(self.data))
+def test_expression_sorting(parser, data):
+    s = ExpressionSorting(parser, "record * (record - 4)")
+    assert s(data) == [2, 3, 1, 4, 0, 5, 6, 7, 8, 9]
 
-    def testExpressionSortingWithDate(self):
-        s = ExpressionSorting(self.parser, "abs((record - date('2011/01/01')).days)")
-        data = [date(2011, 2, 1), date(2010, 12, 15), date(2011, 9, 5), date(2010, 10, 2)]
-        self.assertEqual(
-            [date(2010, 12, 15), date(2011, 2, 1), date(2010, 10, 2), date(2011, 9, 5)],
-            s(data))
+def test_expression_sorting_with_date(parser):
+    s = ExpressionSorting(parser, "abs((record - date('2011/01/01')).days)")
+    data = [date(2011, 2, 1), date(2010, 12, 15),
+            date(2011, 9, 5), date(2010, 10, 2)]
+    assert s(data) == [date(2010, 12, 15), date(2011, 2, 1),
+                       date(2010, 10, 2), date(2011, 9, 5)]
