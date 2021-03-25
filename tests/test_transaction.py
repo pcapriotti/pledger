295c3f28bb8903d2e8150fb688f08ea6a52241c4
Paolo Capriotti <paolo@capriotti.io>
2021-03-25 16:18:29 +0100

    Port test_transaction to pytest

--- tests/test_transaction.py
+++ tests/test_transaction.py
@@ -1,120 +1,126 @@
-import unittest
 from pledger.transaction import Transaction, UnbalancedTransaction, UndefinedTransaction
 from pledger.value import Value, ZERO
 from pledger.entry import Entry
 from pledger.parser import Parser, MalformedHeader
+import pytest
+import re
 
-class TransactionTest(unittest.TestCase):
-    def setUp(self):
-        self.parser = Parser()
-        self.bank = self.parser.accounts["Assets"]["Bank"]
-        self.expenses = self.parser.accounts["Expenses"]["Books"]
 
-    def testParsing(self):
-        str = "2011/04/15 * Bookshop\n" + \
-              "    Assets:Bank       -40 EUR\n" + \
-              "    Expenses:Books"
-        value = self.parser.parse_value("40 EUR")
-        entry1 = Entry(self.bank, -value)
-        entry2 = Entry(self.expenses, value)
-        tr = Transaction([entry1, entry2])
-        self.assertEqual(tr, self.parser.parse_transaction(str))
+@pytest.fixture
+def bank(parser):
+    return parser.accounts["Assets"]["Bank"]
 
-    def testParsingComments(self):
-        str = "; gift for wife\n" + \
-              "2011/05/21 * Bookshop\n" + \
-              "    Assets:Bank      -40 EUR\n" + \
-              "    Expenses:Books"
-        tr = self.parser.parse_transaction(str)
+@pytest.fixture
+def expenses(parser):
+    return parser.accounts["Expenses"]["Books"]
 
-        value = self.parser.parse_value("40 EUR")
-        self.assertEqual("Assets:Bank", tr.entries[0].account.name)
-        self.assertEqual("Expenses:Books", tr.entries[1].account.name)
-        self.assertEqual(-value, tr.entries[0].amount)
-        self.assertEqual(value, tr.entries[1].amount)
+def test_parsing(parser, bank, expenses):
+    str = "2011/04/15 * Bookshop\n" + \
+          "    Assets:Bank       -40 EUR\n" + \
+          "    Expenses:Books"
+    value = parser.parse_value("40 EUR")
+    entry1 = Entry(bank, -value)
+    entry2 = Entry(expenses, value)
+    tr = Transaction([entry1, entry2])
+    assert parser.parse_transaction(str) == tr
 
-    def testEmptyTransaction(self):
-        str = ""
-        self.assertIsNone(self.parser.parse_transaction(str))
+def test_parsing_comments(parser):
+    str = "; gift for wife\n" + \
+          "2011/05/21 * Bookshop\n" + \
+          "    Assets:Bank      -40 EUR\n" + \
+          "    Expenses:Books"
+    tr = parser.parse_transaction(str)
 
-        str = "; hello world"
-        self.assertIsNone(self.parser.parse_transaction(str))
+    value = parser.parse_value("40 EUR")
+    assert tr.entries[0].account.name == "Assets:Bank"
+    assert tr.entries[1].account.name == "Expenses:Books"
+    assert tr.entries[0].amount == -value
+    assert tr.entries[1].amount == value
 
-    def testParseUnbalanced(self):
-        str = "2011/02/05 * Bookshop\n" + \
-              "    Assets:Bank          -40 EUR\n" + \
-              "    Expenses:Book        41 EUR"
-        with self.assertRaises(UnbalancedTransaction) as cm:
-            self.parser.parse_transaction(str)
+def test_empty_transaction(parser):
+    str = ""
+    assert parser.parse_transaction(str) is None
 
-    def testParseUndefined(self):
-        str = "2011/02/05 * Bookshop\n" + \
-              "    Assets:Bank\n" + \
-              "    Expenses:Book"
-        with self.assertRaises(UndefinedTransaction) as cm:
-            self.parser.parse_transaction(str)
+    str = "; hello world"
+    assert parser.parse_transaction(str) is None
 
-    def testMalformedHeader(self):
-        str = "test 2011/02/05 * Bookshop\n" + \
-              "    Assets:Bank          -40 EUR\n" + \
-              "    Expenses:Book"
-        with self.assertRaises(MalformedHeader) as cm:
-            self.parser.parse_transaction(str)
+def test_parse_unbalanced(parser):
+    str = "2011/02/05 * Bookshop\n" + \
+          "    Assets:Bank          -40 EUR\n" + \
+          "    Expenses:Book        41 EUR"
+    with pytest.raises(UnbalancedTransaction):
+        parser.parse_transaction(str)
 
-    def testMalformedHeader2(self):
-        str = " 2011/02/05 * Bookshop\n" + \
-              "    Assets:Bank          -40 EUR\n" + \
-              "    Expenses:Book"
-        with self.assertRaises(MalformedHeader) as cm:
-            self.parser.parse_transaction(str)
+def test_parse_undefined(parser):
+    str = "2011/02/05 * Bookshop\n" + \
+          "    Assets:Bank\n" + \
+          "    Expenses:Book"
+    with pytest.raises(UndefinedTransaction):
+        parser.parse_transaction(str)
 
-    def testUnbalanced(self):
-        value = self.parser.parse_value("40 EUR")
-        entry1 = Entry(self.bank, value)
-        entry2 = Entry(self.expenses, value)
-        self.assertRaises(UnbalancedTransaction, Transaction, [entry1, entry2])
+def test_malformed_header(parser):
+    str = "test 2011/02/05 * Bookshop\n" + \
+          "    Assets:Bank          -40 EUR\n" + \
+          "    Expenses:Book"
+    with pytest.raises(MalformedHeader):
+        parser.parse_transaction(str)
 
-    def testUndefined(self):
-        entry1 = Entry(self.bank, None)
-        entry2 = Entry(self.expenses, None)
-        self.assertRaises(UndefinedTransaction, Transaction, [entry1, entry2])
+def test_malformed_header2(parser):
+    str = " 2011/02/05 * Bookshop\n" + \
+          "    Assets:Bank          -40 EUR\n" + \
+          "    Expenses:Book"
+    with pytest.raises(MalformedHeader):
+        parser.parse_transaction(str)
 
-    def testDefined(self):
-        value = self.parser.parse_value("40 EUR")
-        entry1 = Entry(self.bank, value)
-        entry2 = Entry(self.expenses, None)
-        tr = Transaction([entry1, entry2])
-        self.assertEqual(-value, tr.entries[-1].amount)
+def test_unbalanced(parser, bank, expenses):
+    value = parser.parse_value("40 EUR")
+    entry1 = Entry(bank, value)
+    entry2 = Entry(expenses, value)
+    with pytest.raises(UnbalancedTransaction):
+        Transaction([entry1, entry2])
 
-    def testRepr(self):
-        value = self.parser.parse_value("50 EUR")
-        entry1 = Entry(self.bank, value)
-        entry2 = Entry(self.expenses, None)
-        tr = Transaction([entry1, entry2])
-        self.assertRegex(repr(tr), "Transaction")
+def test_undefined(bank, expenses):
+    entry1 = Entry(bank, None)
+    entry2 = Entry(expenses, None)
+    with pytest.raises(UndefinedTransaction):
+        Transaction([entry1, entry2])
 
-    def testTransactionEntries(self):
-        value = self.parser.parse_value("50 EUR")
-        entry1 = Entry(self.bank, value)
-        entry2 = Entry(self.expenses, None)
-        tr = Transaction([entry1, entry2])
-        self.assertEqual(entry1, tr[0])
-        self.assertEqual(entry2, tr[1])
+def test_defined(parser, bank, expenses):
+    value = parser.parse_value("40 EUR")
+    entry1 = Entry(bank, value)
+    entry2 = Entry(expenses, None)
+    tr = Transaction([entry1, entry2])
+    assert tr.entries[-1].amount == -value
 
-    def testAmount(self):
-        value = self.parser.parse_value("40 EUR")
-        entry1 = Entry(self.bank, value)
-        entry2 = Entry(self.expenses, None)
-        tr = Transaction([entry1, entry2])
-        self.assertEqual(ZERO, tr.amount)
+def test_repr(parser):
+    value = parser.parse_value("50 EUR")
+    entry1 = Entry(bank, value)
+    entry2 = Entry(expenses, None)
+    tr = Transaction([entry1, entry2])
+    assert re.search("Transaction", repr(tr))
 
-    def testClone(self):
-        value = self.parser.parse_value("40 EUR")
-        entry1 = Entry(self.bank, value)
-        entry2 = Entry(self.expenses, None)
-        tr = Transaction([entry1, entry2])
-        tr2 = tr.clone()
-        self.assertFalse(id(tr) == id(tr2))
-        self.assertItemsEqual(tr.entries, tr2.entries)
-        self.assertEqual(tr.date, tr2.date)
-        self.assertEqual(tr.label, tr2.label)
+def test_transaction_entries(parser, bank, expenses):
+    value = parser.parse_value("50 EUR")
+    entry1 = Entry(bank, value)
+    entry2 = Entry(expenses, None)
+    tr = Transaction([entry1, entry2])
+    assert tr[0] == entry1
+    assert tr[1] == entry2
+
+def test_amount(parser, bank, expenses):
+    value = parser.parse_value("40 EUR")
+    entry1 = Entry(bank, value)
+    entry2 = Entry(expenses, None)
+    tr = Transaction([entry1, entry2])
+    assert tr.amount == ZERO
+
+def test_clone(parser, bank, expenses):
+    value = parser.parse_value("40 EUR")
+    entry1 = Entry(bank, value)
+    entry2 = Entry(expenses, None)
+    tr = Transaction([entry1, entry2])
+    tr2 = tr.clone()
+    assert id(tr) != id(tr2)
+    assert set(tr.entries) == set(tr2.entries)
+    assert tr.date == tr2.date
+    assert tr.label == tr2.label
